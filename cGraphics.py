9c2c269cfe77fdf358ce6dbb92af94ba0e687ad0
wenm0tt <105743827+wenm0tt@users.noreply.github.com>
2022-11-09 09:40:00 -0500

    commit

--- cGraphics.py
+++ cGraphics.py
@@ -3,6 +3,8 @@ from matplotlib import *
 import numpy as np
 from numpy import *
 from sympy import *
+from scipy.interpolate import * 
+import math
 
 class cGraphics():
     def plotZW(frame, expression = "z**k", max_K = 10, grid_density = 5, grid = False):                                    
@@ -40,7 +42,7 @@ class cGraphics():
     def find_resulting_root(guess, f, df, roots):
         i=0
         closest_root = roots[0]
-        while i < 5:
+        while i < 3:
             guess = guess - f(guess)/df(guess)
             closest_root = roots[cGraphics.find_nearest(roots,guess)]
             i+=1
@@ -54,39 +56,39 @@ class cGraphics():
         idx = (np.abs(array - value)).argmin()
         return idx
         
-    def plotNV(frame, expression = "z", resolution = 100,dynamic_rendering = False):
+    def custom_taylor(expression_string, iterates=15, initx=0):
         z = Symbol("z")
-        exp = sympify(expression)
-        f = lambdify(z, exp)
-        df = lambdify(z, diff(exp))
+        f = sympify(expression_string)
+        # an array that holds all derivatives of the function.
+        # at index 0 it holds the function, at index 1 it holds first deriv, etc.
+        diffs = []
+        df = f
+        for i in range(iterates):
+            diffs.append(df)
+            df = diff(df)
 
-        roots_instance_a = solve(exp, z)
-        for i in range(len(roots_instance_a)):
-            roots_instance_a[i] = complex(roots_instance_a[i])
+        f = 0
+        for i in range(iterates):
+            f += ((diffs[i].subs(z,initx))*(z-initx)**i)/math.factorial(i) 
+        print(simplify(f))
+        return simplify(f)
 
-        if dynamic_rendering == False:
-            k = resolution - 1
-        else:
-            k = 1
+    def plotNV(roots_instance_a, x1, x2, y1, y2, f,df, expression = "z", resolution = 100):
+        
+        x = np.linspace(x1 - 0.00001, x2 + 0.00002, resolution)
+        y = np.linspace(y1 - 0.00002, y2 + 0.00001, resolution)
+        X,Y = np.meshgrid(x,y)
+        Z = X + 1j * Y  
+        Q = np.empty_like(Z, dtype=np.float64)
 
-        for n in range(k,resolution,10):
-            frame.cla()
-            x = np.linspace(-0.9999, 0.9997, n)
-            y = np.linspace(-1, 0.9998, n)
-            X,Y = np.meshgrid(x,y)
-            Z = X + 1j * Y  
-            Q = np.empty_like(Z, dtype=np.float64)
-
-            Qshape = Q.shape
-            for j in range(Qshape[0]):
-                for k in range(Qshape[1]):
-                    Zk = Z[j,k]
-                    kroot = cGraphics.find_resulting_root(Zk,f,df,roots_instance_a)
-                    Q[j,k] = round((cGraphics.find_nearest(roots_instance_a, kroot)*3/len(roots_instance_a)),3)
-            im = frame.imshow(Q, cmap="plasma", interpolation="bilinear")
-            matplotlib.pyplot.axis('off')
-            matplotlib.pyplot.pause(0.001)
-        matplotlib.pyplot.show()
+        Qshape = Q.shape
+        for j in range(Qshape[0]):
+            for k in range(Qshape[1]):
+                Zk = Z[j,k]
+                kroot = cGraphics.find_resulting_root(Zk,f,df,roots_instance_a)
+                Q[j,k] = round((cGraphics.find_nearest(roots_instance_a, kroot)/len(roots_instance_a)),3)
+        
+        return Q
 
     def plot3D():
         pass
--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-from context import *
+from cGraphics import *
+import tkinter as tk
+import customtkinter as ctk
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
 
 matplotlib.pyplot.style.use('dark_background')
 
-frame_1 = matplotlib.pyplot.subplot()
-cGraphics.plotNV(frame_1, resolution = 1000, expression="z**4-1",dynamic_rendering=True)
+# THINGS TO ADD
+#   NUM ITERATIONS
+#   COLOR SCHEME SETTINGS
+
+INITIAL_ZOOM = 1
+DESIRED_ZOOM = 500
+DESIRED_RESOLUTION = 2000
+DESIRED_EXP = "sin(z**2)*z-1"
+ZOOM = False
+z = Symbol("z")
+exp = sympify(DESIRED_EXP)
+f = lambdify((z), exp)
+df = lambdify(z, diff(exp))
+
+z = Symbol("z")
+exp = cGraphics.custom_taylor(DESIRED_EXP)
+roots_instance_a = solve(exp, z,check=false)
+for i in range(len(roots_instance_a)):
+    roots_instance_a[i] = complex((roots_instance_a[i]))
+
+im = matplotlib.pyplot.imshow(cGraphics.plotNV(roots_instance_a, -3,3,-3,3,f,df,expression=DESIRED_EXP,resolution=DESIRED_RESOLUTION), cmap = "Greys", interpolation="gaussian")
+matplotlib.pyplot.axis('off')
+
+if ZOOM == True:
+    for i in range(INITIAL_ZOOM,DESIRED_ZOOM):
+        im.set_data(cGraphics.plotNV(roots_instance_a,100/-i,100/i,100/-i,100/i,f,df,expression=DESIRED_EXP,resolution=DESIRED_RESOLUTION))
+        matplotlib.pyplot.pause(0.001)
+        matplotlib.pyplot.axis('off')
+
+matplotlib.pyplot.show()
+# app.mainloop()
+
+
 
